2cd5d6e93018530318e4f31ea2bb234eea7c85b6
RMenendez <rmgiacomini@gmail.com>
2025-06-29 19:46:05 +0200

    Add resta(a, b) to conf2

--- conflict.py
+++ conflict.py
@@ -3,3 +3,10 @@ def suma(x, y):
     Returns the sum of x and y.
     """
     return x + y
+
+def resta(a, b):
+    """
+    Returns the difference of a and b.
+    Devuelvel la resta de a y b.
+    """
+    return a - b
